16c9d9ee139404c48748a23ec867fe8a9724d26c
Nikita Plyusnin <me@plyusnin.dev>
2025-06-08 21:28:06 +0300

    add preview_image as attachment and description as rich text to Course model

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,6 +5,14 @@ RSpec.describe Course, type: :model do
     it { is_expected.to belong_to(:teacher).class_name('User').with_foreign_key('teacher_id') }
   end
 
+  describe "attachments" do
+    it { is_expected.to have_one_attached(:preview_image) }
+  end
+
+  describe "rich text" do
+    it { is_expected.to have_rich_text(:description) }
+  end
+
   describe "validations" do
     it { is_expected.to validate_presence_of(:title) }
   end
