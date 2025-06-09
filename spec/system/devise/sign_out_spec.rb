3a33cd05c7f6c70a9a04f5727b26204508f8a3a3
Nikita Plyusnin <me@plyusnin.dev>
2025-06-09 18:31:33 +0300

    add devise sign out system spec

---
+++ spec/system/devise/sign_out_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "Sign out button", type: :system do
+  let(:user) { create(:user) }
+
+  before do
+    sign_in user
+  end
+
+  it "allows a user to log out" do
+    visit root_path
+    click_link I18n.t("devise.sessions.new.sign_out")
+
+    expect(page).not_to have_link(I18n.t("devise.sessions.new.sign_out"))
+    expect(page).to have_current_path(new_user_session_path)
+  end
+
+  it "redirects to the root path after logging out" do
+    visit root_path
+    click_link I18n.t("devise.sessions.new.sign_out")
+
+    expect(current_path).to eq(new_user_session_path)
+  end
+end
