65993815df1c10685d2bde9961c6ec64fcc6c78e
Nikita Plyusnin <me@plyusnin.dev>
2025-06-11 17:00:17 +0300

    add students association to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,8 @@
 class Course < ApplicationRecord
   belongs_to :teacher, class_name: "User", foreign_key: "teacher_id"
   has_many :lessons, dependent: :destroy
+  has_many :student_courses, dependent: :destroy
+  has_many :students, through: :student_courses, class_name: "User", foreign_key: "student_id"
 
   has_one_attached :preview_image
   has_rich_text :description
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,6 +5,14 @@ require "rails_helper"
 RSpec.describe Course, type: :model do
   describe "associations" do
     it { is_expected.to belong_to(:teacher).class_name("User").with_foreign_key("teacher_id") }
+
+    it { is_expected.to have_many(:student_courses).dependent(:destroy) }
+    it {
+      is_expected.to have_many(:students)
+        .through(:student_courses)
+        .class_name("User")
+        .with_foreign_key("student_id")
+    }
   end
 
   describe "attachments" do
