62ed4d6803dbc6adb10a1962e2e81a5c14710e00
Nikita Plyusnin <me@plyusnin.dev>
2025-06-13 15:10:45 +0300

    add requests sessions sign in spec

---
+++ spec/requests/api/v1/users/sessions_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require "swagger_helper"
+
+describe "User Sessions API", type: :request do
+  path "/api/v1/users/sign_in" do
+    let(:registred_user) { create(:user) }
+
+    post "Sign in a user" do
+      tags "User Sessions"
+      consumes "application/json"
+      produces "application/json"
+
+      parameter name: :body, in: :body, schema: {
+        type: :object,
+        properties: {
+          user: {
+            type: :object,
+            properties: {
+              email: { type: :string, format: :email },
+              password: { type: :string }
+            },
+            required: %w[email password]
+          }
+        }
+      }
+
+      response "201", "User signed in successfully" do
+        let(:body) do
+          {
+            user: {
+              email: registred_user.email,
+              password: registred_user.password
+            }
+          }
+        end
+
+        schema type: :object,
+               properties: {
+                 user: {
+                   type: :object,
+                   properties: {
+                     id: { type: :integer },
+                     email: { type: :string, format: :email },
+                     created_at: { type: :string, format: :date_time },
+                     updated_at: { type: :string, format: :date_time }
+                   }
+                 }
+               }
+
+        run_test!
+      end
+
+      response "401", "Invalid credentials" do
+        let(:body) do
+          {
+            user: {
+              email: registred_user.email,
+              password: "wrong_password"
+            }
+          }
+        end
+
+        run_test!
+      end
+    end
+  end
+end
