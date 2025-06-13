e87b5c810c6df84bd1bd22a7f2f3f351b07c34e8
Nikita Plyusnin <me@plyusnin.dev>
2025-06-13 15:10:24 +0300

    installed rswag

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
+
   devise_for :users
 
   namespace :api do
