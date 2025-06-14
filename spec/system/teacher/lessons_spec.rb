deeb56db42e2df7e02523520656aa742b62347fe
Nikita Plyusnin <me@plyusnin.dev>
2025-06-14 21:57:36 +0300

    add specs for teacher lessons feature

--- spec/system/teacher/lessons_spec.rb
+++ spec/system/teacher/lessons_spec.rb
@@ -10,6 +10,33 @@ RSpec.describe "Teacher/Lessons", type: :system do
     sign_in(teacher)
   end
 
+  describe "Teacher visits lessons list page" do
+    let!(:lessons) { create_list(:lesson, 2, course: course) }
+
+    before do
+      visit teacher_course_lessons_path(course)
+    end
+
+    it "displays the lessons list" do
+      lessons.each do |lesson|
+        expect(page).to have_content(lesson.title)
+      end
+    end
+  end
+
+  describe "Teacher visit a lesson page" do
+    let(:lesson) { create(:lesson, course: course) }
+
+    before do
+      visit teacher_lesson_path(lesson)
+    end
+
+    it "displays the lesson details" do
+      expect(page).to have_content(lesson.title)
+      expect(page).to have_content(lesson.content)
+    end
+  end
+
   describe "Teacher creates a new lesson" do
     before do
       visit new_teacher_course_lesson_path(course)
@@ -35,6 +62,53 @@ RSpec.describe "Teacher/Lessons", type: :system do
         expect(page).to have_content(I18n.t("errors.messages.blank"))
         expect(course.reload.lessons.count).to eq(0)
       end
+    end
+  end
+
+  describe "Teacher edits a lesson" do
+    let!(:lesson) { create(:lesson, course: course) }
+
+    before do
+      visit edit_teacher_lesson_path(lesson)
+    end
+
+    context "with valid parameters" do
+      let(:updated_lesson_params) { attributes_for(:lesson, title: "Updated Lesson Title") }
+
+      it "update lesson and render updated lesson" do
+        fill_in I18n.t("activerecord.attributes.lesson.title"), with: updated_lesson_params[:title]
+        click_button I18n.t("resources.teacher.lessons.save_as_draft")
+
+        expect(page).to have_content(I18n.t("notices.teacher.lessons.updated"))
+        expect(page).to have_content(updated_lesson_params[:title])
+        expect(lesson.reload.title).to eq(updated_lesson_params[:title])
+      end
+    end
+
+    context "with invalid parameters" do
+      it "not update lesson and render errors" do
+        fill_in I18n.t("activerecord.attributes.lesson.title"), with: ""
+        click_button I18n.t("resources.teacher.lessons.save_as_draft")
+
+        expect(page).to have_content(I18n.t("errors.messages.blank"))
+        expect(lesson.reload.title).not_to eq("")
+      end
+    end
+  end
+
+  describe "Teache deletes a lesson" do
+    let!(:lesson) { create(:lesson, course: course) }
+
+    before do
+      visit edit_teacher_lesson_path(lesson)
+    end
+
+    it "successfully deletes a lesson" do
+      click_link I18n.t("resources.teacher.lessons.destroy")
+
+      expect(page).to have_content(I18n.t("notices.teacher.lessons.destroyed"))
+      expect(page).not_to have_content(lesson.title)
+      expect(course.reload.lessons.count).to eq(0)
     end
   end
 end
